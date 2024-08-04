507ef19323c1d7f1338d38938539a62b977405ff
A1exKonst <94910859+A1exKonst@users.noreply.github.com>
2024-08-04 10:44:31 +0300

    Create Button.py

---
+++ Interfaces/Button.py
@@ -0,0 +1,14 @@
+from Abstract_UI_Element import Abstract_UI_Element
+
+class Button(Abstract_UI_Element):
+    def __init__(self):
+        pass
+
+    def CreateQtObject(self):
+        pass
+
+    def OnPush(self):
+        pass
+
+    def OnRelease(self):
+        pass
