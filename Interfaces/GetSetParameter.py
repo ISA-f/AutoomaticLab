ebb369af1ea84dd0860f08ad40b29f589cb579c0
A1exKonst <94910859+A1exKonst@users.noreply.github.com>
2024-08-04 10:40:41 +0300

    Create GetSetParameter.py

---
+++ Interfaces/GetSetParameter.py
@@ -0,0 +1,14 @@
+from Abstract_UI_Element import Abstract_UI_Element
+
+class GetSetParameter(Abstract_UI_Element):
+    def __init__(self):
+        pass
+
+    def CreateQtObject(self):
+        pass
+
+    def Get(self):
+        pass
+
+    def Set(self, value):
+        pass
